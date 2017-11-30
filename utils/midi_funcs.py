e748316c63bcffb2bcc807a0d1d385e3bb681888
Jacob Sundstrom <jacob.sundstrom@gmail.com>
2017-11-30 15:38:23 -0800

    New funcs and tests

--- utils/midi_funcs.py
+++ utils/midi_funcs.py
@@ -1,10 +1,10 @@
 # parse a MIDI file into vectors of equal length for pitch, rhythm, and velocity
 from mido import MidiFile
+import numpy as np
 
 # only works with monophonic instruments. Returns an matrix
 def simple_parse_midi(midifile, trackNum, tempo):
   # mid = MidiFile(midifile) # read in a file
-  noteEvents = [] # empty list for note events
   pitches = [] # list for pitches; rests are -1
   velocities = [] # list for velocities of pitches; rests are 127
   rhythms = [] # list of rhythmic units
@@ -25,6 +25,63 @@ def simple_parse_midi(midifile, trackNum, tempo):
 
   return([pitches, rhythms, velocities])
 
+# the size of the array returned is always the number of notes - 1
+def get_intervals(pitches):
+  intervals = []
+  no_rests = []
+  for pitch in pitches:
+    if pitch>0:
+      no_rests.append(pitch)
+  for i, pitch in enumerate(no_rests):
+    if i>0:
+      intervals.append(no_rests[i] - no_rests[i-1])
+  return (intervals)
+
+# trying to make phrases
+def get_phrases(midifile, trackNum, tempo):
+  pitches = np.array([]) # list for pitches; rests are -1
+  velocities = [] # list for velocities of pitches; rests are 127
+  rhythms = [] # list of rhythmic units
+  phrase = np.array([]) # an empty phrase
+
+  for i, msg in enumerate(midifile.tracks[trackNum]):
+    if msg.type=='note_on':
+      this_rhythm = get_rhythm(msg.time, midifile.ticks_per_beat) * -1 # negative since the last beat was a rest
+      if this_rhythm < -0.1: # eliminate short rests (non-existent rests)
+        rhythms.append(this_rhythm)
+        # phrase.append(-1) # a rest, so no note but it's in our phrase
+        np.append(phrase, [-1])
+        velocities.append(0) # no velocity
+      if this_rhythm < -3:
+        # else it's a long rest so we make a new phrase and add the old one to our pitches
+        # pitches.append(phrase)
+        np.append(pitches, phrase.flatten())
+        phrase = np.array([])
+
+    if msg.type=='note_off':
+      this_rhythm = get_rhythm(msg.time, midifile.ticks_per_beat) # negative since the last beat was a rest
+      rhythms.append(this_rhythm) # remember the duration of the note
+      # phrase.append(msg.note) # remember the pitch
+      np.append(phrase, [msg.note])
+      velocities.append(msg.velocity) # remember the loudness of that note
+
+    print(phrase)
+
+  # pitches.append(phrase) # make sure we append the last phrase (double check that we didn't already)
+  np.append(pitches, phrase.flatten())
+
+  # shape = (len(pitches), len(max(pitches,key=len)))
+  # pitches = [np.array(phrase) for phrase in pitches]
+  # pitches = np.array(pitches)
+  # sigma = np.diag(pitches)
+  # sigma.resize(shape)
+
+  # zeros = np.zeros(shape, dtype=np.int32)
+  # zeros[:sigma.shape[0], :sigma.shape[1]] = sigma
+  # pitches = sigma.asarray
+
+  return([pitches, rhythms, velocities])
+
 def get_tempo(midifile):
   for track in midifile.tracks:
     for msg in track:
@@ -32,11 +89,71 @@ def get_tempo(midifile):
           return(msg.tempo)
 
 def get_rhythm(time, ticks_per_beat):
-    return (time/ticks_per_beat)
+    return (time/ticks_per_beat) # beat is 16th notes???
 
 def make_ticks(rhythm, ticks_per_beat):
     return(rhythm*ticks_per_beat)
 
-# check key and transpose accordingly
-def check_key(mid, desiredKey):
-  pass
+# check key
+def get_key(midifile):
+  for track in midifile.tracks:
+    for msg in track:
+      if msg.type=='key_signature':
+        if msg.key=='C':
+          key = 0
+        elif msg.key=='C#':
+          key = 1
+        elif msg.key=='D':
+          key = 2
+        elif msg.key=='D#':
+          key = 3
+        elif msg.key=='E':
+          key = 4
+        elif msg.key=='F':
+          key = 5
+        elif msg.key=='F#':
+          key = 6
+        elif msg.key=='G':
+          key = 7
+        elif msg.key=='Ab':
+          key = 8
+        elif msg.key=='A':
+          key = 9
+        elif msg.key=='A#':
+          key = 10
+        else:
+          key = 11
+    return (key) # return the key
+
+
+def make_time_series(midifile, out_path):
+  # midifile.ticks_per_beat
+  time_series = [] # an empty list
+  this_file = open(out_path, "w") # open a file for writing
+
+  # write a header
+  this_file.write(str(get_tempo(midifile))) # write the get_tempo
+  this_file.write(" ") # a space
+  this_file.write(str(midifile.ticks_per_beat)) # ticks per beat
+  this_file.write(" ") # a space
+  this_file.write("0 0 0 0 ") # four zeros
+
+  for track in midifile.tracks:
+    for i, msg in enumerate(track):
+
+      # it's a rest!
+      if msg.type=="note_on":
+        # write msg.time number of 'samples'
+        for _ in range(msg.time):
+          this_file.write(str(-1)) # a rest is -1
+          this_file.write(" ") # a space
+
+      # it's a note!
+      if msg.type=="note_off":
+        for _ in range(msg.time):
+          this_file.write(str(msg.note))
+          this_file.write(" ") # a space
+
+  this_file.close
+
+  return (out_path) # return the out_path
--- workbench/experiments/testScript.py
+++ workbench/experiments/testScript.py
@@ -1,10 +1,55 @@
 from mido import MidiFile
 from utils.midi_funcs import *
+import numpy as np
 
-mid = MidiFile('midi/MilesDavis_SoWhat_FINAL.mid')
+midifile = MidiFile('midi/MilesDavis_SoWhat_FINAL.mid')
+track = midifile.tracks[0]
+#
+# tempo = get_tempo(mid)
+# print(tempo)
+#
+parsed = simple_parse_midi(midifile, 0, get_tempo(midifile))
+# print(parsed)
+#
+# sig = get_key(mid)
+# print(sig)
 
-tempo = get_tempo(mid)
-print(tempo)
+# thisFile = open("./midiFeatures.txt", "w")
+# features = ' '.join(str(e) for e in parsed[0])
+# thisFile.write(features)
+# thisFile.write("\n")
+# features = ' '.join(str(e) for e in parsed[1])
+# thisFile.write(features)
+# thisFile.write("\n")
+# features = ' '.join(str(e) for e in parsed[2])
+# thisFile.write(features)
+# thisFile.close
+# print(parsed)
 
-parsed = simple_parse_midi(mid, 0, tempo)
-print(parsed)
+#
+# noteEvents = [] # empty list for note events
+# pitches = [] # list for pitches; rests are -1
+# velocities = [] # list for velocities of pitches; rests are 127
+# rhythms = [] # list of rhythmic units
+# measure = []
+# time_sum = 0
+# intervals = []
+#
+# for msg in track:
+#   if msg.type=='note_on':
+#     pitches.append(msg.note)
+#
+# for i, pitch in enumerate(pitches):
+#   if i>0:
+#     intervals.append(pitches[i] - pitches[i-1])
+#
+# # print (intervals)
+# intervals = get_intervals(parsed[0])
+# # print(intervals)
+#
+# parsed = get_phrases(midifile, 0, get_tempo(midifile))
+# for phrase in parsed[0]:
+#   print(phrase)
+# print(parsed[0])
+
+make_time_series(midifile, "./time_series")
